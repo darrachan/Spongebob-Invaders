63ba632e66da3632d579c2a5c9d56feb4a44fe1c
Kian Thompson <kian.s.thompson@gmail.com>
2019-02-11 19:35:42 -0800

    Added Pause Menu
    - pause menu during gameplay
    - moved shooting mechanic to Update() so that no key presses are missed

--- Space Invaders/Assets/Script/PlayerController.cs
+++ Space Invaders/Assets/Script/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     public LineRenderer bullet;
     public static bool canShoot = true;
     public static int lives = 3;
+    public Transform pauseMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // none
+        if (canShoot && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            Instantiate(projectile, new Vector3(projectileLocation.transform.position.x, projectileLocation.transform.position.y, 1), projectileLocation.rotation); //projectileLocation.position
+            canShoot = false;
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Escape)))
+        {
+            Pause();
+        }
     }
 
     void Restart(){
@@ -39,15 +50,32 @@ public class PlayerController : MonoBehaviour
             transform.position += Vector3.right * -playerSpeed * Time.deltaTime;
         }
 
+        // moved this because it wasn't registering space inputs all the time (downside of using FixedUpdate)
+        /*
         if (canShoot && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
         {
             Instantiate(projectile, new Vector3(projectileLocation.transform.position.x, projectileLocation.transform.position.y, 1), projectileLocation.rotation); //projectileLocation.position
             canShoot = false;
         }
+        */
+    }
+
+    private void Pause()
+    {
+        if (pauseMenu.gameObject.activeInHierarchy == false)
+        {
+            pauseMenu.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenu.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 
 
-
+    //We should try and not use FixedUpdate because it processes frames in different ways (no frames, many, or few frames per second)
     private void FixedUpdate()
     {
         Move();
